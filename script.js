f36afae01fd44fb056fed85385df4917408aae36
JBrickhouse <jbrickhouse777@gmail.com>
2020-07-16 14:43:52 -0500

    Random password success

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 
 
@@ -16,6 +16,7 @@ var dataV1 = {
 
 // Initialize the password length as 0
 var passwordLength = 0;
+
 // Ask the user for a password length.
 // It repeats the prompt until they enter a value that is within of 8 to 128 characters.
 // It also repeats the prompt if a user enters something that is not a number
@@ -37,102 +38,109 @@ var userWantSpecial = confirm("Do you want special characters? \n !, ?, #, etc. 
 var initialArray = new Array(Number(passwordLength))
 
 
-// Proof that I can populate values within an array using a for loop
+// Using a for loop to loop through the initialArray and assign each index with a random character, based on the user's input preferences from before
 for (i=0; i < initialArray.length; i++) {
-    initialArray[i]=i;
-}
-
-// These functions below will generate random whole numbers within a range
-// The range corresponds to the possible index values of the variables in question
-// For example: dataV1.uppercase has a length of 26.
-// The random numbers will be between 0 and 25, inclusive.
-// That means I use that random number to select a value from within that array
-// Done with assistance from Mozilla developer notes: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// Done with assstance from this Stack Overflow thread: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-var upperCaseRandom = Math.floor(Math.random() * Math.floor(dataV1.upperCase.length));
-var lowerCaseRandom = Math.floor(Math.random() * Math.floor(dataV1.lowerCase.length));
-var numbersRandom = Math.floor(Math.random() * Math.floor(dataV1.numbers.length));
-var specialRandom = Math.floor(Math.random() * Math.floor(dataV1.special.length));
-// Selecting a value from within the array, using the random numbers generated above
-var upperCaseChar = dataV1.upperCase[upperCaseRandom];
-var lowerCaseChar = dataV1.lowerCase[lowerCaseRandom];
-var numbersChar = dataV1.numbers[numbersRandom];
-var specialChar = dataV1.special[specialRandom];
+    
+    // These functions below will generate random whole numbers within a range
+    // The range corresponds to the possible index values of the variables in question
+    // For example: dataV1.uppercase has a length of 26.
+    // The random numbers will be between 0 and 25, inclusive.
+    // That means I use that random number to select a value from within that array
+    // Done with assistance from Mozilla developer notes: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    // Done with assstance from this Stack Overflow thread: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+    var upperCaseRandom = Math.floor(Math.random() * Math.floor(dataV1.upperCase.length));
+    var lowerCaseRandom = Math.floor(Math.random() * Math.floor(dataV1.lowerCase.length));
+    var numbersRandom = Math.floor(Math.random() * Math.floor(dataV1.numbers.length));
+    var specialRandom = Math.floor(Math.random() * Math.floor(dataV1.special.length));
+    // Selecting a value from within the array, using the random numbers generated above
+    var upperCaseChar = dataV1.upperCase[upperCaseRandom];
+    var lowerCaseChar = dataV1.lowerCase[lowerCaseRandom];
+    var numbersChar = dataV1.numbers[numbersRandom];
+    var specialChar = dataV1.special[specialRandom];
 
 
-// Based on what the user assembled, create an array of those random characters
+    // Based on the parameters the user selected, create an mini-array of those random characters, entitled characterSelectionArray
+    // (Note: I feel like there should be a more simple way to accomplish this... This feels a little brute force, as I have manually listed all possible combinations of the user parameters.)
 
-// User selected the following: Uppercase
-if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === false) {
-    var characterSelectionArray = [upperCaseChar];
-}
-// User selected the following: Lowercase
-else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === false && userWantSpecial === false) {
-    var characterSelectionArray = [lowerCaseChar];    
-}
-// User selected the following: Numeric
-else if (userWantLowerCase === false && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === false) {
-    var characterSelectionArray = [numbersChar];
-}
-// User selected the following: Special Characters
-else if (userWantLowerCase === false && userWantUpperCase === false && userWantNumbers === false && userWantSpecial === true) {
-    var characterSelectionArray = [specialChar];    
-}
-// User selected the following: Lowercase, Uppercase
-else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === false) {
-    var characterSelectionArray = [upperCaseChar, lowerCaseChar];    
-}
-// User selected the following: Lowercase, Numeric
-else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === false) {
-    var characterSelectionArray = [lowerCaseChar, numbersChar];    
-}
-// User selected the following: Lowercase, Special Characters
-else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === false && userWantSpecial === true) {
-    var characterSelectionArray = [lowerCaseChar, specialChar];    
-}
-// User selected the following: Uppercase, Numeric
-else if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === false) {
-    var characterSelectionArray = [upperCaseChar, numbersChar];    
-}
-// User selected the following: Uppercase, Special Characters
-else if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === true) {
-    var characterSelectionArray = [upperCaseChar, specialChar];    
-}
-// User selected the following: Numeric, Special Characters
-else if (userWantLowerCase === false && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === true) {
-    var characterSelectionArray = [numbersChar, specialChar];    
-}
-// User selected the following: Lowercase, Uppercase, Numeric
-else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === false) {
-    var characterSelectionArray = [upperCaseChar, lowerCaseChar, numbersChar];    
-}
-// User selected the following: Lowercase, Uppercase, Special Characters
-else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === true) {
-    var characterSelectionArray = [upperCaseChar, lowerCaseChar, specialChar];    
-}
-// User selected the following: Lowercase, Numeric, Special Characters
-else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === true) {
-    var characterSelectionArray = [lowerCaseChar, numbersChar, specialChar];    
-}
-// User selected the following: Uppercase, Numeric, Special Characters
-else if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === true) {
-    var characterSelectionArray = [upperCaseChar, numbersChar, specialChar];    
-}
-// User selected the following: Lowercase, Uppercase, Numeric, Special Characters
-else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === true) {
-    var characterSelectionArray = [upperCaseChar, lowerCaseChar, numbersChar, specialChar];    
+    // User selected the following: Uppercase
+    if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === false) {
+        var characterSelectionArray = [upperCaseChar];
+    }
+    // User selected the following: Lowercase
+    else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === false && userWantSpecial === false) {
+        var characterSelectionArray = [lowerCaseChar];    
+    }
+    // User selected the following: Numeric
+    else if (userWantLowerCase === false && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === false) {
+        var characterSelectionArray = [numbersChar];
+    }
+    // User selected the following: Special Characters
+    else if (userWantLowerCase === false && userWantUpperCase === false && userWantNumbers === false && userWantSpecial === true) {
+        var characterSelectionArray = [specialChar];    
+    }
+    // User selected the following: Lowercase, Uppercase
+    else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === false) {
+        var characterSelectionArray = [upperCaseChar, lowerCaseChar];    
+    }
+    // User selected the following: Lowercase, Numeric
+    else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === false) {
+        var characterSelectionArray = [lowerCaseChar, numbersChar];    
+    }
+    // User selected the following: Lowercase, Special Characters
+    else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === false && userWantSpecial === true) {
+        var characterSelectionArray = [lowerCaseChar, specialChar];    
+    }
+    // User selected the following: Uppercase, Numeric
+    else if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === false) {
+        var characterSelectionArray = [upperCaseChar, numbersChar];    
+    }
+    // User selected the following: Uppercase, Special Characters
+    else if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === true) {
+        var characterSelectionArray = [upperCaseChar, specialChar];    
+    }
+    // User selected the following: Numeric, Special Characters
+    else if (userWantLowerCase === false && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === true) {
+        var characterSelectionArray = [numbersChar, specialChar];    
+    }
+    // User selected the following: Lowercase, Uppercase, Numeric
+    else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === false) {
+        var characterSelectionArray = [upperCaseChar, lowerCaseChar, numbersChar];    
+    }
+    // User selected the following: Lowercase, Uppercase, Special Characters
+    else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === false && userWantSpecial === true) {
+        var characterSelectionArray = [upperCaseChar, lowerCaseChar, specialChar];    
+    }
+    // User selected the following: Lowercase, Numeric, Special Characters
+    else if (userWantLowerCase === true && userWantUpperCase === false && userWantNumbers === true && userWantSpecial === true) {
+        var characterSelectionArray = [lowerCaseChar, numbersChar, specialChar];    
+    }
+    // User selected the following: Uppercase, Numeric, Special Characters
+    else if (userWantLowerCase === false && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === true) {
+        var characterSelectionArray = [upperCaseChar, numbersChar, specialChar];    
+    }
+    // User selected the following: Lowercase, Uppercase, Numeric, Special Characters
+    else if (userWantLowerCase === true && userWantUpperCase === true && userWantNumbers === true && userWantSpecial === true) {
+        var characterSelectionArray = [upperCaseChar, lowerCaseChar, numbersChar, specialChar];    
+    }
+    // User selected the following: NONE
+    // (Ends the program because the user has not selected ANY of the character options
+    else {
+        break;
+    }
+
+    // This function below will generate a random whole number within a range
+    // The range corresponds to the possible index values of the variables in question
+    var characterSelectRandom = Math.floor(Math.random() * Math.floor(characterSelectionArray.length));
+    // Using the random number generated above, select a character from within the mini-array characterSelectionArray
+    var characterSelect = characterSelectionArray[characterSelectRandom];
+    
+    // Taking that selected character and passing into the current index (based on the for loop) of initialArray
+    initialArray[i]=characterSelect;
 }
 
-// This function below will generate a random whole number within a range
-// The range corresponds to the possible index values of the variables in question
-var characterSelectRandom = Math.floor(Math.random() * Math.floor(characterSelectionArray.length));
-// Selecting a value from within the array, using the random number generated above
-var characterSelect = characterSelectionArray[characterSelectRandom];
-
-
-
-// PUT ALL THE NUMBERS INTO AN ARRAY
-// CONCATENATE THE ARRAY AT THE END
+// Concatenate the initialArray into one long string (with no spaces), thus generating the finalArray, to be used as the password
+// Done with the assistance of the Mozilla developer notes: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+var finalArray = initialArray.join("");
 
 
 
@@ -142,11 +150,11 @@ var characterSelect = characterSelectionArray[characterSelectRandom];
 
 
 // Write password to the #password input
-// function writePassword() {
-//     var password = generatePassword();
-//     var passwordText = document.querySelector("#password");
-//     passwordText.value = password;
-// }
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
